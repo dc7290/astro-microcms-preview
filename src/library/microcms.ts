c0c675fcb8888aa7d1a2c8f4e083b931d1e720c0
dc7290 <dhkh.cba0927@gmail.com>
2022-12-19 18:18:33 +0900

    libからlibraryにファイル名変更

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -2,8 +2,6 @@
 import { createClient, MicroCMSQueries } from "microcms-js-sdk";
 
 const client = createClient({
-  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
 //型定義
